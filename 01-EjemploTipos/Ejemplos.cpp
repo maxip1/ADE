c91b274f7bdfd34079d170f4f407172811972a9d
maxip1 <82190060+maxip1@users.noreply.github.com>
2021-05-06 21:25:42 -0300

    Update Ejemplos.cpp

--- 01-EjemploTipos/Ejemplos.cpp
+++ 01-EjemploTipos/Ejemplos.cpp
@@ -10,36 +10,38 @@ Peña Maximiliano Ezequiel
 
 int main (){
     
-    bool bool1{true}, bool2{false};
-    assert (bool1 and bool2 == false); //Por que no pasa la prueba con assert (false == bool1 and bool2)  ???
-    assert ( true == bool1 or bool2);
-    assert (1 == bool1 or bool2 and bool2);    // Primero operación and luego or   / true=1; false=0
+    //Bool
+    assert (true and false == false);
+    assert ( true == true or false);
+    assert (true == true or false and false);    // Primero operación and luego or   / true=1; false=0
 
-    char car1{'A'}, car2{'C'};      // A = 65 , C = 67
-    assert (132 == car1 + car2);
-    assert (4355 == car1 * car2);
-    assert (-2 == car1 - car2);
+    //Char
+    assert ('C' == 'A' + 2);
+    assert (1 == 'D' - 'C');
+    assert ( 'a' > 'A');
 
-    unsigned nat1{20}, nat2{3};
-    assert (2 == nat1 % nat2); // 20/3 --> resto 2
-    assert (60 == nat1 * nat2);
-    assert (23 == nat1 + nat2);
+    //Unsigned
+    assert (2u == 20u % 3u); //Con el sufijo "u" en c++ se considera un tipo de dato unsigned
+    assert (60u == 20u * 3u);
+    assert (23u == 20u + 3u);
 
-    int ent1{-5}, ent2{3};
-    assert(-8 == ent1 - ent2);
-    assert(8 == ent2 - ent1);
-    assert(-15 == ent1 * ent2);
+    //Int
+    assert(-8 == -5 - 3);
+    assert(8 == 3 - -5);
+    assert(-15 == -5 * 3);
 
-    double rea1{1.5}, rea2{3.5};
-    assert (5.25 == rea1 * rea2);
-    assert (true == rea1 < rea2);
-    assert (-2 == rea1 - rea2);
-    //assert (2.33 == rea2 / rea1);  //Ver redondeo y tolerancia
+    //Double
+    assert (5.25 == 1.5 * 3.5);
+    assert (true == 1.5 < 3.5);
+    assert (-2.0 == 1.5 - 3.5);
+    //assert (6.66 == 20.0 / 3.0);
+    //assert (2.33  == 3.5 / 1.5);  //Ver redondeo y tolerancia
 
-    std::string cad1{"Hola"}, cad2{"mundo!"};
-    assert ("Hola mundo!" == cad1 + " " +cad2);
-    assert (4 == cad1.length());
-    assert (-2 == cad1.length() - cad2.length());
+    //String
+    assert (std::string ("Hola mundo!") == std::string ("Hola") + " " + std::string ("mundo!"));
+    assert (4 == std::string ("Hola").length());
+    assert (std::string ("Hola").length() < std::string ("mundo!").length());
 
 }
+
 
